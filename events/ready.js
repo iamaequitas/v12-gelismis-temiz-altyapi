a86d0e8ccf9ba8777cc0811c715d440162deede8
Mehmet Bora <75122785+Ryhtus@users.noreply.github.com>
2021-01-07 19:51:32 +0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`
+∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+                 ${client.user.username}
+∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
+`);
+  client.user.setStatus("online");
+  client.user.setActivity("", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
+};
