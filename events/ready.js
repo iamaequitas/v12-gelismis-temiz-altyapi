7bd94e042d28f212a549bcd349dbb8ddf2ef5370
aequitas <75122785+AEQUlTAS@users.noreply.github.com>
2021-01-27 23:37:39 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,14 +3,12 @@ const moment = require('moment');
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
 
-var prefix = ayarlar.prefix;
-
 module.exports = client => {
   console.log(`
 ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
-                 ${client.user.username}
+         ${client.user.username}
 ∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
 `);
   client.user.setStatus("online");
-  client.user.setActivity("", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
+  client.user.setActivity("bot durum kısmınız", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
 };
