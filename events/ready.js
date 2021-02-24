37cf0ba7813f418cc90fd844a222b4a60650292b
aequitas <75122785+AEQUlTAS@users.noreply.github.com>
2021-02-24 11:32:01 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,26 +1,20 @@
 const Discord = require('discord.js');
-
 module.exports = client => {
-  console.log(`
-∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
-         ${client.user.username}
-∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞
-`);
+ 
 client.on("ready", () => {
-  client.user.setPresence({
-    game: {
-      name: "Aequitas v12 altyapı",
-      type: "WATCHING"
-      // url: 'https://www.twitch.tv/amerikaniks'
-      // Değerler:
-      // PLAYING: Oynuyor
-      // WATCHING: İzliyor
-      // LISTENING: Dinliyor
-    },
-    status: "idle"
-    // Değerler:
-    // online: Çevrimiçi
-    // dnd: Rahatsız Etmeyin
-    // idle: Boşta
-  });
-});
+client.user.setPresence({
+game: {
+name: "Aequitas v12 altyapı",
+type: "WATCHING"
+url: 'https://www.twitch.tv/aequitasaltyapı'
+// Değerler:
+// PLAYING: Oynuyor
+// WATCHING: İzliyor
+// LISTENING: Dinliyor
+},
+status: "idle"
+// Değerler:
+// online: Çevrimiçi
+// dnd: Rahatsız Etmeyin
+// idle: Boşta
+});});}
