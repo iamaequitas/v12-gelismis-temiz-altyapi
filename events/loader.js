abdb8c3caeb1153f3eb2ca80ad2b83ea462d0ddb
aequitas <75122785+iamaequitas@users.noreply.github.com>
2021-06-13 18:33:54 +0300

    Update loader.js

--- events/loader.js
+++ events/loader.js
@@ -1,5 +1,2 @@
 const reqEvent = (event) => require(`../events/${event}`);
-module.exports = client => {
-  client.on('ready', () => reqEvent('ready')(client));
-  client.on('message', reqEvent('message'));
-};
+module.exports = client => { client.on('message', reqEvent('message')) };
