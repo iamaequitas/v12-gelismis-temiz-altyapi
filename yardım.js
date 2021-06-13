1e3aee26c61ba8a6c05a87480dcd70bec5dd98f9
aequitas <75122785+iamaequitas@users.noreply.github.com>
2021-06-13 18:40:35 +0300

    Update and rename komut/komutlar.js to yardım.js

---
+++ yardım.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+exports.run = async (client, message, args) => {
+	
+message.channel.send(`Bota İstediğiniz Modülü Ve Komudu Ekleyebilirsiniz Botu Şekillendirmek Size Kalmış İyi Kullanımlar aequitas ∞#9999`)
+   
+}
+exports.komut = { name: 'aequitas', aliases: ["yardım"], cooldown: 3};
